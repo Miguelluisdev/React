5ee6233a0b8d9c766dbd9188dafa83f4529700a2
Miguel Luis <miguelluisataideferreira68@gmail.com>
2023-11-19 16:56:30 -0300

    z

---
+++ my-project/src/components/Frase.jsx
@@ -0,0 +1,8 @@
+function Frase(){
+  return (
+    <div className="className">
+      <p>My first Component</p>
+    </div>
+  )
+}
+export default Frase
--- my-project/src/components/HelloWorld.jsx
+++ my-project/src/components/HelloWorld.jsx
@@ -1,11 +1,12 @@
-
+import Frase from './frase'
 
 function HelloWorld(){
 
   return (
     <div className="btn" >
+      <frase/>
       <h1>Hello World</h1>
-      
+      <Frase></Frase>
     </div>
   )
 }
