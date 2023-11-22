045061823767477baaa052470afd7a9b85e75b79
Miguel Luis <miguelluisataideferreira68@gmail.com>
2023-11-22 18:31:46 -0300

    eventos, rest and children

--- ex3/latest/src/components/Profile/index.jsx
+++ ex3/latest/src/components/Profile/index.jsx
@@ -1,18 +1,28 @@
+/* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable react/prop-types */
+import Title from "../Title"
+import ProfileSection from "./ProfileSection"
 import styles from "./styles.module.css"
+
 export default function Profile(props) {
   return (
     <div className={styles.container}>
       <img className={styles.avatar} src={props.avatar} alt={props.name} />
-      <h2 className={styles.name}>{props.name}</h2>
-      <div>{props.bio}</div>
-      <div>{props.phone}</div>
-      <div>{props.email}</div>
-      <div className={styles.links}>
-        <a href={props.githubUrl} target="_blank" rel="noreferrer">GitHub</a>
-        <a href={props.linkedinUrl} target="_blank" rel="noreferrer">LinkedIn</a>
-        <a href={props.twitterUrl} target="_blank" rel="noreferrer">Twitter</a>
-      </div>
+      <Title>
+        <span>Name: </span>
+        {props.name}
+        <button>Follow</button>
+      </Title>
+      <ProfileSection>{props.bio}</ProfileSection>
+      <ProfileSection>{props.phone}</ProfileSection>
+      <ProfileSection>{props.email}</ProfileSection>
+      <ProfileSection>
+				<div className={styles.links}>
+	        <a href={props.githubUrl} target="_blank">GitHub</a>
+	        <a href={props.linkedinUrl} target="_blank">LinkedIn</a>
+	        <a href={props.twitterUrl} target="_blank">Twitter</a>
+				</div>
+      </ProfileSection>
     </div>
   )
 }
