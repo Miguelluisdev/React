01c7508f0d7064decbc75b9be8e00f7580be616e
Miguel Luis <miguelluisataideferreira68@gmail.com>
2023-11-30 20:08:43 -0300

    gerador de senha

--- gerador-senha/gerador/src/App.jsx
+++ gerador-senha/gerador/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react"
 function App() {
-  const [count, setCount] = useState(0)
+  const [password,setPassword] = useState("")
+  const [copy,  setCopyText] = useState("Copiar")
 
-  return (
-    <>
+  function generate(){
+    const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
+    const length = 12
+    let newPassword = ""
+    for (let i = 0; i < length; i++) {
+        const position = Math.floor(Math.random() * characters.length)
+      newPassword += characters[position]
+
+    }
+    setPassword(newPassword)
+    setCopyText("copiar")
+  }
+
+  
+	function copyToClipboard() {
+    window.navigator.clipboard.writeText(password)
+    setCopyText("Copiado!")
+  }
+
+
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Gerador de senha</h1>
+        <button onClick={generate} >Gerar!</button>
+        <button onClick={copyToClipboard} >{copy}</button>
+        <div>{password}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
 }
 
 export default App
