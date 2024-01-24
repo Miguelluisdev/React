fdf9341c7ed8a505a3e8aa2e2629ee9083c1addc
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-01-23 22:58:01 -0300

    react-router

--- react-router/react-router/src/App.jsx
+++ react-router/react-router/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes , Route , Link , NavLink } from "react-router-dom"
 import Home from "./components/Home"
 import About from "./components/About"
 import Contact from "./components/Contact"
+import Login from "./components/Login"
+import Dashboard from "./components/Dashboard"
 export default function App() {
 
 
@@ -13,6 +15,9 @@ export default function App() {
         <li  ><Link to="/" >Inicio</Link></li>
         <li><Link to="/about" >Sobre</Link></li>
         <li> <Link to="/contact" >Contato</Link></li>
+        <li><NavLink  className={({isActive}) => (isActive ? "active-link" : "")} to="/contact" ></NavLink></li>
+        <li><Link to="/login" >Login</Link></li>
+        <li><Link to="/dashboard" >dashboard</Link></li>
     </ul>
     </nav>
 
@@ -21,6 +26,8 @@ export default function App() {
         < Route  path="/" element={<Home/>} />
       <Route path="/about" element={<About/>} />
         < Route  path="/Contact" element={<Contact/>}  />
+        < Route path="/login" element={<Login/>} />
+        < Route path="/dashboard" element={<Dashboard/>} />
       </Routes>
     
     
