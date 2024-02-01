c5435c7f2309a23e2b8e5c296463967a6dd2f0ff
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-02-01 15:29:34 -0300

    tailwind

--- ts/src/components/BarraFerramentas.tsx
+++ ts/src/components/BarraFerramentas.tsx
@@ -1,17 +1,25 @@
-import { useContext } from "react"
-import { TemaContext } from "../contexts/TemaContext"
-const BarraFerramentas = () => {
+import { useContext } from "react";
+import { TemaContext } from "../contexts/TemaContext";
 
-  const contextoTema = useContext(TemaContext)
+const BarraFerramentas = () => {
+  const contextoTema = useContext(TemaContext);
 
   if (!contextoTema) {
-    return null
+    return null;
   }
-  return (
-    <div style={{ background: contextoTema.tema === "claro" ? '#FFF' : '#000', color: contextoTema.tema === "claro" ? '#000' : "#FFF" }} >
-      <button onClick={contextoTema.alternarTema} >Alterar Tema</button>
-    </div>
-  )
-}
 
-export default BarraFerramentas
+  return (
+    <div style={{ background: contextoTema.tema === "claro" ? "#FFF" : "#000", color: contextoTema.tema === "claro" ? "#000" : "#FFF" }}>
+      <button onClick={contextoTema.alternarTema}>Alterar Tema</button>
+      <h1 className="text-3xl font-normal underline text-red-800 ">
+        Hello world!
+      </h1>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Funcionar
+      </button>
+
+    </div>
+  );
+};
+
+export default BarraFerramentas;
--- ts/src/components/Contador.tsx
+++ ts/src/components/Contador.tsx
@@ -5,7 +5,7 @@ import { counterReducer } from "./CounterReducer"
 const Contador = () => {
   const [count, dispatch] = useReducer(counterReducer, 0)
   return (
-    <div>
+    <div className="text-xl via-blue-900 from-blue-600">
       Contador: {count}
       <button onClick={() => dispatch({ type: "increment" })} >Incrementar</button>
       <button onClick={() => dispatch({ type: "decrement" })} ></button>
