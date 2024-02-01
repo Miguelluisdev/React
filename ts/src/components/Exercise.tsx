cf7f8cf4a3c73babf4aef1fdfa5771c0aba9e2a6
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-02-01 20:14:30 -0300

    tailwind

--- ts/src/components/Exercise.tsx
+++ ts/src/components/Exercise.tsx
@@ -11,12 +11,12 @@ const Exercise = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="usuario">Nome do usuario</label>
+      <div className="container bg-gray-600 text-yellow-400 backdrop:border-r-blue-900" >
+        <label htmlFor="usuario" className="font-serif font text-2xl" >Nome do usuario</label>
         <input type="text" value={usuario} onChange={(e) => setUsuario(e.target.value)} />
       </div>
       <div>
-        <label htmlFor="usuario">Senha:</label>
+        <label htmlFor="usuario" className="font-serif font text-2xl" >Senha:</label>
         <input type="text" value={senha} onChange={(e) => setSenha(e.target.value)} />
       </div>
       <button type="submit" >entrar</button>
